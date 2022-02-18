dbbf9c6b60d3c4cf687f5b43ce2ee03342e62e9c
GRILLO Benjamin <94958182+GRILLO-Benjamin@users.noreply.github.com>
2022-02-18 11:56:17 +0100

    !!

--- classes/Personnage.php
+++ classes/Personnage.php
@@ -20,14 +20,14 @@ Class Character{
         $cible->vie -= $this->atk;
     }
 
-    public function regenerer($vie = null){
-        if(is_null($vie)){
-            $this->vie = 100;
-        } else {
-            $this->vie += $vie;
-        }
-        $this->vie = 100;
-    }
+    // public function regenerer($vie = null){
+    //     if(is_null($vie)){
+    //         $this->vie = 100;
+    //     } else {
+    //         $this->vie += $vie;
+    //     }
+    //     $this->vie = 100;
+    // }
 
     public function mort(){
         return $this->vie <= 0;

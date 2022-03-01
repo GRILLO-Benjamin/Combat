d773f470f98f0fae880dc0e29814601e2f9a2b01
GRILLO Benjamin <94958182+GRILLO-Benjamin@users.noreply.github.com>
2022-03-01 13:36:54 +0100

    selected character js ok

--- JS/main.js
+++ JS/main.js
@@ -1,31 +1,18 @@
-let select = document.querySelectorAll("#charac");
-
-// function characChoice(){
-//       
-// }
-select.forEach(image => {
-    image.addEventListener("click", () => {
-        let imgSelected = document.querySelector('#charac');
-        console.log(imgSelected);
-        image.style.backgroundColor = "green";
-    })
-    image.addEventListener("click", () => {
-        console.log(image);
-        image.style.backgroundColor = "green";
-    })
-});
-
-// si a = image active (clicked)
-// bg color = green
-// si b = new image active (clicked)
-// bg color = green and a bg color is back to origiin
-
-
-// select.addEventListener('click', function(){
-// });
-
-
 //to do : onclick on the characters img 
-//        i want to keep displaying the hoover css
+//        i want to change the bg color only on the selected img
+
+let select = document.querySelectorAll("#charac");
+let image = document.getElementsByClassName("character_img");
+
+for (let i = 0; i < image.length; i++) {
+    let currentImage = image[i];
+    currentImage.onclick = function () {
+        select.forEach(function(el){
+            el.classList.remove('selected-img')
+        });
+        this.classList.add('selected-img')
+    }
+}
+
 //        then on submit on the button i want to display it in my carousel
 

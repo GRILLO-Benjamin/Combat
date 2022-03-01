4d5d82176dad105738165ec9dc79dbca460fdac1
Xavier Coënen-Corbo <x.coenen.dev@gmail.com>
2022-03-01 16:11:25 +0100

    mardi hello

--- classes/Character.php
+++ classes/Character.php
@@ -6,35 +6,59 @@
 ■ frapper un autre personnage
 ■ recevoir des dégâts. -->
 <?php 
+
 Class Character{
+    //ATTRIBUTS
+    private $_vie = 100;
+    private  $_atk = 40;
+    private $_name = "Unknown";
+    private $_classe = "Unknown";
 
-    public $vie = 100;
-    public $atk = 10;
-    public $nom;
-
-    public function __construct($nom){
-        $this->nom = $nom;
-
-    }
-    public function attaque ($cible){
-        $cible->vie -= $this->atk;
-    }
-
-    // public function regenerer($vie = null){
-    //     if(is_null($vie)){
-    //         $this->vie = 100;
-    //     } else {
-    //         $this->vie += $vie;
-    //     }
-    //     $this->vie = 100;
-    // }
-
-    public function mort(){
-        return $this->vie <= 0;
+    //CONSTRUCTEUR
+    public function __construct($name, $vie, $atk, $classe){
+        $this->_name = $name;
+        $this->_vie = $vie;
+        $this->setAttack ($atk);
+        $this->setClasse ($classe);
     }
 
-    public function vie(){
-        return $this->vie;
+    //METHODES
+    public function getAttack (){
+    return $this->atk;
     }
+
+    public function setAttack($atk){
+    $this->_atk= $atk;
+    }
+
+
+public function setClasse($classe){
+    return $this->_classe = $classe;
+}
+
+public function getInfo (){
+    return "<p>".$this->_name. " a une force de " .$this->_atk. " est de classe ".$this->_classe. ".</p>";
+}
+
+public function frapper(Character $character){
+return $character->recevoirDegats($this->_atk);
+}
+
+public function recevoirDegats($atk){
+$this->_vie = $this->_vie - $atk;
+
+if($this->_vie <= 0){
+    echo "<p>" .$this->_name." a perdu " .$atk. " points de vie. Il vient de succomber à ses blessures.</p>";
+}else {
+
+echo "<p>" .$this->_name." a perdu " .$atk. " points de vie. il lui reste ".$this->_vie. " points.</p>";
+}
+}
 
 }
+
+$warrior = new Character($vie, 45, "Conan", $classe);
+$rogue = new Character($vie, 30, "Lupin", $classe);
+$mage = new Character($vie, 35,  "Merlin", $classe);
+$ranger = new Character($vie, 40, "Robin", $classe);
+
